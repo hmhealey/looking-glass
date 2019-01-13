544615a0c3e8379e0aaaf1e5e67e0b42ab6e50c2
Harrison Healey <harrisonmhealey@gmail.com>
2019-01-13 15:05:26 -0500

    Added cards.idsByName reducer

--- shared/reducers/cards.js
+++ shared/reducers/cards.js
@@ -20,6 +20,26 @@ export function byId(state = {}, action) {
     }
 }
 
+export function idsByName(state = {}, action) {
+    switch(action.type) {
+        case ActionTypes.RECEIVED_CARD:
+            console.log('idsByName', action.card.name, action.card.id);
+            return {
+                ...state,
+                [action.card.name]: action.card.id,
+            };
+        case ActionTypes.RECEIVED_CARDS:
+            return action.cards.reduce((nextState, card) => ({
+                ...nextState,
+                [card.name]: card.id,
+            }), state);
+
+        default:
+            return state;
+    }
+}
+
 export default combineReducers({
     byId,
+    idsByName,
 });
